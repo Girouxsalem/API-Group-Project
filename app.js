cf51f5d78d0112229ff7903354206511ccf55e17
Girouxsalem <girouxclaude27@hotmail.com>
2022-08-22 19:49:40 +0100

    Added api url, key and id

--- app.js
+++ app.js
@@ -3,4 +3,12 @@
 // API KEY:  0415e12cec4a9f7b67521a618de23066
 // App ID: b6985f44
 
+const apiUrl = "https://od-api.oxforddictionaries.com/api/v2/";
+const apiKey = "0415e12cec4a9f7b67521a618de23066";
+const appId = "b6985f44";
+
+
+
+
+
 
