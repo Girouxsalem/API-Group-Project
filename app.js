f1d0357abd4f142af34d827875ababb3cfd2db16
Girouxsalem <girouxclaude27@hotmail.com>
2022-08-23 21:01:30 +0100

    Changed from generate to search feature and finished styling

--- app.js
+++ app.js
@@ -6,15 +6,6 @@ const result = document.getElementById("result");
 const generate = document.getElementById("generate");
 const soundBtn = document.querySelector(".speech");
 
-generate.addEventListener("click", () => {
-    fetch(`${url}`).then((response) => response.json) .then((data) => console.log(data));
-});
-
-soundBtn.addEventListener("click", () => {
-    let utterance = new SpeechSynthesisUtterance(`${wordDisplay.innerText}`);
-    speechSynthesis.speak(utterance);
-    // SpeechSynthesisUtterance is a web speech api that represents a speech request
-});
 
 
 
